d35310ffd869b7b48a0c61638e4ed556295ad92d
dev-jeffers <devsfull.jefferson@gmail.com>
2024-09-14 02:41:27 -0400

    update name package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="meu_primeiro_pacote",
+    name="meu_primeiro_pacote.1",
     version="0.0.2",
     author="Jefferson",
     author_email="devsfull.jefferson@gmail.com",
